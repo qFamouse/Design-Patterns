6eac64093a61bcb0c92ebe4f6b8432efe0ee3445
qFamouse <famouse_ru@mail.ru>
2022-02-17 19:38:04 +0300

    Adding Constant Matrix class

---
+++ DesignPatterns.SOLID/MatrixCalculator/ConstantMatrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.SOLID.MatrixCalculator.Interfaces;
+
+namespace DesignPatterns.SOLID.MatrixCalculator
+{
+    public class ConstantMatrix : IMatrix
+    {
+        private readonly int _value;
+        public int Size { get; }
+
+        public ConstantMatrix(int value, int size)
+        {
+            SizeIsValid(size);
+            _value = value;
+            Size = size;
+        }
+
+        public int Get(int i, int j)
+        {
+            IndexWithinOfRange(i, j);
+            return _value;
+        }
+        private void IndexWithinOfRange(in int i,in int j)
+        {
+            if (i >= Size || i < 0)
+            {
+                throw new IndexOutOfRangeException("Incorrect indexes");
+            }
+        }
+
+        private void SizeIsValid(in int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentException("Size must be positive");
+            }
+        }
+    }
+}
