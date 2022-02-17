ddd3e947dc63d0972e132cdac32713f2a0dd2962
qFamouse <famouse_ru@mail.ru>
2022-02-17 19:36:55 +0300

    Add MatrixCalculator and basic interfaces for classes

---
+++ DesignPatterns.SOLID/MatrixCalculator/Interfaces/IMatrix.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.MatrixCalculator.Interfaces
+{
+    public interface IMatrix
+    {
+        int Size { get; }
+        int Get(int i, int j);
+    }
+}
---
+++ DesignPatterns.SOLID/MatrixCalculator/Interfaces/IMutableMatrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.MatrixCalculator.Interfaces
+{
+    public interface IMutableMatrix : IMatrix
+    {
+        void Set(int i, int j, int value);
+    }
+}
