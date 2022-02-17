cd319454ee9fb9a864c22da000da56ee08d31cdd
qFamouse <famouse_ru@mail.ru>
2022-02-17 19:35:49 +0300

    Move to separate directory

---
+++ Design-Patterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DesignPatterns.SOLID.MatrixCalculator;
+using DesignPatterns.SOLID.MatrixCalculator.Interfaces;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UsualMatrix a = new UsualMatrix(new int[2, 2]
+            {
+                {2, 2},
+                {2, 2}
+            });
+
+            ConstantMatrix b = new ConstantMatrix(1, 2);
+
+            var c = b + a;
+
+            var s = 5 + 5;
+        }
+    }
+}
