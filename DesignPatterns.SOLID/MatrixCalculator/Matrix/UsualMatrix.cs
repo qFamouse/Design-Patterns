338bb8e8d385710090d1d1ecf7843b588e3dbfed
qFamouse <famouse_ru@mail.ru>
2022-02-19 15:05:54 +0300

    Change UsualMatrix

---
+++ DesignPatterns.SOLID/MatrixCalculator/Matrix/UsualMatrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesignPatterns.SOLID.MatrixCalculator.Matrix
+{
+    public sealed class UsualMatrix : BaseMatrix
+    {
+        public override int Size { get; }
+
+        private readonly double[,] _matrix;
+
+        public UsualMatrix(double[,] matrix)
+        {
+            MatrixIsNotNull(matrix);
+            IsSquareMatrix(matrix);
+
+            _matrix = matrix;
+            Size = matrix.GetLength(0);
+        }
+
+        public UsualMatrix(int size)
+        {
+            Size = size;
+            _matrix = new double[size, size];
+        }
+
+        public override double Get(int i, int j) => _matrix[i, j];
+
+        public override void Set(int i, int j, double value) => _matrix[i, j] = value;
+
+        #region Validation
+
+        private static void IsSquareMatrix(double[,] matrix)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+            {
+                throw new ArgumentException("This matrix is not square");
+            }
+        }
+
+        private static void MatrixIsNotNull(double[,] matrix)
+        {
+            if (matrix is null)
+            {
+                throw new NullReferenceException("Matrix is null");
+            }
+        }
+
+        private static void EqualSizeValidation(int size1, int size2)
+        {
+            if (size1 != size2)
+            {
+                throw new ArithmeticException("The dimensions of the matrix are not equal");
+            }
+        }
+
+        #endregion
+    }
+}
