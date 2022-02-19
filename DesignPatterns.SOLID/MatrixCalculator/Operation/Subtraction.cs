07192350029c2de2626ca940e67b39f3448d0604
qFamouse <famouse_ru@mail.ru>
2022-02-19 17:00:18 +0300

    Subtraction for matrix

---
+++ DesignPatterns.SOLID/MatrixCalculator/Operation/Subtraction.cs
@@ -0,0 +1,34 @@
+﻿using DesignPatterns.SOLID.MatrixCalculator.Matrix;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SOLID.MatrixCalculator.Operation
+{
+    public class Subtraction : IOperation
+    {
+        public BaseMatrix Calc(BaseMatrix a, BaseMatrix b)
+        {
+            int size = a.Size;
+            if (size != b.Size)
+            {
+                throw new ArgumentException("The dimensions of the matrix must be equal");
+            }
+
+            BaseMatrix matrix = new UsualMatrix(size);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    double sum = a.Get(i, j) - b.Get(i, j);
+                    matrix.Set(i, j, sum);
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
