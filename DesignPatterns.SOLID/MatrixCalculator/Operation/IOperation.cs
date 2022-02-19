b65c63e87cf17eaf21f556e122995adf7714b7ad
qFamouse <famouse_ru@mail.ru>
2022-02-19 15:07:02 +0300

    Adding IOperation Interface for future operations with the matrixes

---
+++ DesignPatterns.SOLID/MatrixCalculator/Operation/IOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DesignPatterns.SOLID.MatrixCalculator.Matrix;
+
+namespace DesignPatterns.SOLID.MatrixCalculator.Operation
+{
+    public interface IOperation
+    {
+        BaseMatrix Calc(BaseMatrix a, BaseMatrix b);
+    }
+}
