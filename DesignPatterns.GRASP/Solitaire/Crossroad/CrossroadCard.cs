416d4bb72efb4978db86d81f40e51c4d927c2947
Famouse <famouse_ru@mail.ru>
2022-03-16 20:27:53 +0300

    Adding CrossroadCard - inheritor of ICard

---
+++ DesignPatterns.GRASP/Solitaire/Crossroad/CrossroadCard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DesignPatterns.GRASP.Solitaire.Core.Interfaces;
+
+namespace DesignPatterns.GRASP.Solitaire.Crossroad
+{
+    internal class CrossroadCard : ICard
+    {
+        public CardValue Value { get; init; }
+        public CardSuit Suit { get; init; }
+
+        public static bool operator >(CrossroadCard a, CrossroadCard b)
+        {
+            return a.Value > b.Value;
+        }
+
+        public static bool operator <(CrossroadCard a, CrossroadCard b)
+        {
+            return a.Value < b.Value;
+        }
+
+        public static bool operator ==(CrossroadCard a, CrossroadCard b)
+        {
+            return a.Value == b.Value;
+        }
+
+        public static bool operator !=(CrossroadCard a, CrossroadCard b)
+        {
+            return a.Value != b.Value;
+        }
+    }
+}
