f41637e6102be50ad687130082e2fd72aa0e6c72
Famouse <famouse_ru@mail.ru>
2022-03-16 20:26:24 +0300

    Move ICard to interface folder Remove contract to set property

--- DesignPatterns.GRASP/Solitaire/Core/Interfaces/ICard.cs
+++ DesignPatterns.GRASP/Solitaire/Core/Interfaces/ICard.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DesignPatterns.GRASP.Solitaire.Core
+﻿namespace DesignPatterns.GRASP.Solitaire.Core.Interfaces
 {
     internal enum CardValue
     {
@@ -33,7 +27,7 @@ namespace DesignPatterns.GRASP.Solitaire.Core
 
     internal interface ICard
     {
-        CardValue Value { get; set; }
-        CardSuit Suit { get; set; }
+        CardValue Value { get; }
+        CardSuit Suit { get; }
     }
 }
