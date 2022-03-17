8206aa01c82ce13b7ad88378e814569ed8404274
Famouse <famouse_ru@mail.ru>
2022-03-17 22:26:01 +0300

    Complite GameInit method Complite ToString method Some field renaming

--- DesignPatterns.GRASP/Solitaire/Crossroads/SolitaireCrossroads .cs
+++ DesignPatterns.GRASP/Solitaire/Crossroads/SolitaireCrossroads .cs
@@ -10,34 +10,50 @@ namespace DesignPatterns.GRASP.Solitaire.Crossroads
 {
     public sealed class SolitaireCrossroads
     {
-        private int _singleCardsCount;
-        private const int CARDS_COUNT = 52;
+        private const int SINGLE_CARDS_COUNT = 4;
+        private const int MAIN_DECK_SIZE = 52;
 
-        private Queue<CrossroadsCard> _mainDeck;
+        private Stack<CrossroadsCard> _reserveStack;
 
         private CrossroadsCard[] _singleCards;
 
-        private Stack<CrossroadsCard> _centerDeck;
+        private Stack<CrossroadsCard> _centralStack;
         
-        public SolitaireCrossroads(int singleCardsCount = 4)
+        public SolitaireCrossroads()
         {
-            _singleCardsCount = singleCardsCount;
+
         }
 
         public void GameInit()
         {
             // Initialization cards //
-            var cards = new List<CrossroadsCard>(CARDS_COUNT);
+            var cards = new List<CrossroadsCard>(MAIN_DECK_SIZE);
 
-            foreach (CardValue value in Enum.GetValues(typeof(CardValue))) {
-                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit))) {
+            foreach (CardValue value in Enum.GetValues(typeof(CardValue)))
+            {
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+                {
                     cards.Add(new CrossroadsCard(value, suit));
                 }
             }
 
             cards.Shuffle();
-            _mainDeck = new Queue<CrossroadsCard>(cards);
+            _reserveStack = new Stack<CrossroadsCard>(cards);
 
+            // Initialize playing cards //
+            _singleCards = new CrossroadsCard[SINGLE_CARDS_COUNT];
+            _singleCards = _singleCards.Select(card => card = _reserveStack.Pop()).ToArray();
+
+            _centralStack = new Stack<CrossroadsCard>();
+            _centralStack.Push(_reserveStack.Pop());
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{_singleCards[0], 15}\n" +
+                $"{_reserveStack.Peek()}{_singleCards[1], 8}{_centralStack.Peek(), 5}{_singleCards[2], 5}\n" +
+                $"{_singleCards[3], 15}\n";
         }
     }
 }
