2a70e742e21c16e2060beda9f4feaef67cb39488
Famouse <famouse_ru@mail.ru>
2022-03-17 21:29:01 +0300

    Fix name mistake SolitaireCrosssroads to SolitaireCrossroads. Adding card initialize

---
+++ DesignPatterns.GRASP/Solitaire/Crossroads/SolitaireCrossroads .cs
@@ -0,0 +1,43 @@
+﻿using DesignPatterns.GRASP.Solitaire.Core.Interfaces;
+using DesignPatterns.GRASP.Solitaire.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.GRASP.Solitaire.Crossroads
+{
+    public sealed class SolitaireCrossroads
+    {
+        private int _singleCardsCount;
+        private const int CARDS_COUNT = 52;
+
+        private Queue<CrossroadsCard> _mainDeck;
+
+        private CrossroadsCard[] _singleCards;
+
+        private Stack<CrossroadsCard> _centerDeck;
+        
+        public SolitaireCrossroads(int singleCardsCount = 4)
+        {
+            _singleCardsCount = singleCardsCount;
+        }
+
+        public void GameInit()
+        {
+            // Initialization cards //
+            var cards = new List<CrossroadsCard>(CARDS_COUNT);
+
+            foreach (CardValue value in Enum.GetValues(typeof(CardValue))) {
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit))) {
+                    cards.Add(new CrossroadsCard(value, suit));
+                }
+            }
+
+            cards.Shuffle();
+            _mainDeck = new Queue<CrossroadsCard>(cards);
+
+        }
+    }
+}
