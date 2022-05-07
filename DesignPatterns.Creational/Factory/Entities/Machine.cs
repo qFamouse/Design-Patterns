4aaae734404780ab2fb90982565d7a863917f8fc
Famouse <famouse_ru@mail.ru>
2022-05-07 20:01:17 +0300

    Adding entities

---
+++ DesignPatterns.Creational/Factory/Entities/Machine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Entities
+{
+    public sealed class Machine
+    {
+        public string Name { get; set; }
+        /// <summary>
+        /// Number of parts per hour
+        /// </summary>
+        public ushort HourProductivity { get; set; }
+        /// <summary>
+        /// Profit from one part
+        /// </summary>
+        public int PieceProfit { get; set; }
+    }
+}
---
+++ DesignPatterns.Creational/Factory/Entities/Repair.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Entities
+{
+    public sealed class Repair
+    {
+        public Machine Machine { get; set; }
+        /// <summary>
+        /// Repair date
+        /// </summary>
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        /// <summary>
+        /// Duration in hours
+        /// </summary>
+        public ushort HoursDuration { get; set; }
+        /// <summary>
+        /// Cost for repair
+        /// </summary>
+        public int Cost { get; set; }
+    }
+}
