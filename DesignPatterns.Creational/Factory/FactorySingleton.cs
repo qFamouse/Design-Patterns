0eb02699d63c5281e9f2b8deebd721b9fc80d245
Famouse <famouse_ru@mail.ru>
2022-05-07 21:58:15 +0300

    Adding Singleton for FactoryReader

---
+++ DesignPatterns.Creational/Factory/FactorySingleton.cs
@@ -0,0 +1,23 @@
+﻿using DesignPatterns.Creational.Factory.Reader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory
+{
+    public class FactorySingleton
+    {
+        private static FactorySingleton instance = new FactorySingleton();
+
+        public FactoryReader FactoryReader { get; }
+
+        private protected FactorySingleton() { }
+
+        public static FactorySingleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
