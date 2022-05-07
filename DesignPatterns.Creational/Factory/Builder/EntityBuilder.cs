58853086bdf3b0ea777213f328c4ad5127229e58
Famouse <famouse_ru@mail.ru>
2022-05-07 20:33:53 +0300

    Adding architecture for creating entities

---
+++ DesignPatterns.Creational/Factory/Builder/EntityBuilder.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatterns.Creational.Factory.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Builder
+{
+    public abstract class EntityBuilder
+    {
+        protected StreamReader StreamReader { get; set; }
+        public EntityBuilder(StreamReader streamReader)
+        {
+            StreamReader = streamReader;
+        }
+        public abstract void ReadMachines(StreamReader stream);
+        public abstract void ReadRenovations(StreamReader stream);
+        public abstract List<Machine> GetMachines();
+    }
+}
---
+++ DesignPatterns.Creational/Factory/Builder/XmlEntityBuilder.cs
@@ -0,0 +1,31 @@
+﻿using DesignPatterns.Creational.Factory.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Builder
+{
+    public sealed class XmlEntityBuilder : EntityBuilder
+    {
+        private List<Machine> _machines;
+        public XmlEntityBuilder(StreamReader streamReader) : base(streamReader) { }
+
+        public override List<Machine> GetMachines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadMachines(StreamReader stream)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReadRenovations(StreamReader stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
