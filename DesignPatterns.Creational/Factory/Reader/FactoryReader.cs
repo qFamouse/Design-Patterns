6d164d8ab84c70169b28bb62799a6d2e628ea80d
Famouse <famouse_ru@mail.ru>
2022-05-07 21:49:57 +0300

    Create factory reader (pattern Factory Method)

---
+++ DesignPatterns.Creational/Factory/Reader/FactoryReader.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.Creational.Factory.Builder;
+using DesignPatterns.Creational.Factory.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Reader
+{
+    public abstract class FactoryReader
+    {
+        public abstract EntityBuilder GetBuilder();
+    }
+}
---
+++ DesignPatterns.Creational/Factory/Reader/XmlFactoryReader.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Creational.Factory.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Factory.Reader
+{
+    public sealed class XmlFactoryReader : FactoryReader
+    {
+        public override EntityBuilder GetBuilder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
