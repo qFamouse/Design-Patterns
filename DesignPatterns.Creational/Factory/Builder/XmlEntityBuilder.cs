e0a20fc5d91fe0baa9fa491e007d15ae89ac61ec
Famouse <famouse_ru@mail.ru>
2022-05-08 16:53:07 +0300

    Comlete xml entity builder

--- DesignPatterns.Creational/Factory/Builder/XmlEntityBuilder.cs
+++ DesignPatterns.Creational/Factory/Builder/XmlEntityBuilder.cs
@@ -1,30 +1,78 @@
 ﻿using DesignPatterns.Creational.Factory.Entities;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DesignPatterns.Creational.Factory.Builder
 {
     public sealed class XmlEntityBuilder : EntityBuilder
     {
-        private List<Machine> _machines;
+        private List<Machine> _machines = new List<Machine>();
 
         public override List<Machine> GetMachines()
         {
-            throw new NotImplementedException();
+            return _machines;
         }
 
         public override void ReadMachines(StreamReader stream)
         {
-            throw new NotImplementedException();
+            var document = new XmlDocument();
+            document.Load(stream);
+
+            XmlNode xmlMachines = document.DocumentElement.SelectSingleNode("/Machines");
+
+            foreach (XmlNode xmlMachine in xmlMachines)
+            {
+                Machine machine = new Machine();
+                foreach (XmlNode xmlNode in xmlMachine.ChildNodes)
+                {
+                    switch (xmlNode.Name)
+                    {
+                        case "Name":
+                            machine.Name = xmlNode.InnerText;
+                            break;
+
+                        case "HourProductivity":
+                            machine.HourProductivity = ushort.Parse(xmlNode.InnerText);
+                            break;
+
+                        case "PieceProfit":
+                            machine.PieceProfit = int.Parse(xmlNode.InnerText);
+                            break;
+
+                        default:
+                            throw new NotSupportedException("Unknown name");
+                    }
+                }
+                machine.Renovations = new List<Renovation>();
+                _machines.Add(machine);
+            }
         }
 
         public override void ReadRenovations(StreamReader stream)
         {
-            throw new NotImplementedException();
+            var document = new XmlDocument();
+            document.Load(stream);
+
+            XmlNode xmlRenovations = document.DocumentElement.SelectSingleNode("/Renovations");
+
+            foreach (XmlNode xmlRenovation in xmlRenovations)
+            {
+                var machineName = xmlRenovation.ChildNodes.Item(0).InnerText;
+
+                _machines.Find(m => m.Name == machineName)?.Renovations.Add(new Renovation()
+                {
+                    Date = DateTime.ParseExact(xmlRenovation.ChildNodes[1].InnerText, "dd.MM.yyyy", CultureInfo.InvariantCulture),
+                    Description = xmlRenovation.ChildNodes[2].InnerText,
+                    HoursDuration = ushort.Parse(xmlRenovation.ChildNodes[3].InnerText),
+                    Cost = int.Parse(xmlRenovation.ChildNodes[4].InnerText)
+                });
+            }
         }
     }
 }
