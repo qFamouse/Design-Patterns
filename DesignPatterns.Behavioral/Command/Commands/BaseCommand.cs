c8fdd7da60e5dac41280c18e416b95040cb168a1
Famouse <famouse_ru@mail.ru>
2022-06-17 20:13:15 +0300

    Adding base command class

---
+++ DesignPatterns.Behavioral/Command/Commands/BaseCommand.cs
@@ -0,0 +1,26 @@
+﻿using DesignPatterns.Behavioral.Command.Entities;
+using DesignPatterns.Behavioral.Command.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Behavioral.Command.Commands
+{
+    public delegate void Receiver(object? value);
+
+    public abstract class BaseCommand : ICommand
+    {
+        private protected BugTrackingSystem bugTrackingSystem;
+        private protected Receiver receiver;
+
+        abstract public void Execute(string[] args);
+
+        protected BaseCommand(BugTrackingSystem bugTrackingSystem, Receiver receiver)
+        {
+            this.bugTrackingSystem = bugTrackingSystem ?? throw new ArgumentNullException(nameof(bugTrackingSystem));
+            this.receiver = receiver ?? throw new ArgumentNullException(nameof(receiver));
+        }
+    }
+}
