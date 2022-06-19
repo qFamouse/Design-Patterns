5d154c75e6f0370a429ea3856efa5b8e5c12951c
Famouse <famouse_ru@mail.ru>
2022-06-19 18:44:42 +0300

    Adding find + renaming in invoker class

--- DesignPatterns.Behavioral/Command/Invoker.cs
+++ DesignPatterns.Behavioral/Command/Invoker.cs
@@ -14,42 +14,50 @@ namespace DesignPatterns.Behavioral.Command
         private Receiver Receiver { get; set; }
 
 
-        public ViewErrorsCommand ViewErrors { get; }
-        public ViewLevelsCommand ViewLevels { get; }
-        public ViewStatusesCommand ViewStatuses { get; }
+        public ViewErrorsCommand ViewErrorsCommand { get; }
+        public ViewLevelsCommand ViewLevelsCommand { get; }
+        public ViewStatusesCommand ViewStatusesCommand { get; }
 
-        public InsertCriticallyLevelCommand InsertCriticallyLevel { get; }
-        public InsertErrorCommand InsertError { get; }
-        public InsertStatusCommand InsertStatus { get; }
+        public InsertCriticallyLevelCommand InsertCriticallyLevelCommand { get; }
+        public InsertErrorCommand InsertErrorCommand { get; }
+        public InsertStatusCommand InsertStatusCommand { get; }
 
-        public EditCriticallyLevelCommand EditCriticallyLevel { get; }
-        public EditErrorCommand EditError { get; }
-        public EditStatusCommand EditStatus { get; }
+        public EditCriticallyLevelCommand EditCriticallyLevelCommand { get; }
+        public EditErrorCommand EditErrorCommand { get; }
+        public EditStatusCommand EditStatusCommand { get; }
 
-        public RemoveCriticallyLevelCommand RemoveCriticallyLevel { get; }
-        public RemoveErrorCommand RemoveError { get; }
-        public RemoveStatusCommand RemoveStatus { get; }
+        public RemoveCriticallyLevelCommand RemoveCriticallyLevelCommand { get; }
+        public RemoveErrorCommand RemoveErrorCommand { get; }
+        public RemoveStatusCommand RemoveStatusCommand { get; }
+
+        public FindErrorsCommand FindErrorsCommand { get; }
+        public FindStatusesCommand FindStatusesCommand { get; }
+        public FindLevelsCommand FindLevelsCommand { get; }
 
         public Invoker(BugTrackingSystem bugTrackingSystem, Receiver receiver)
         {
             BugTrackingSystem = bugTrackingSystem;
             Receiver = receiver;
 
-            ViewLevels = new ViewLevelsCommand(BugTrackingSystem, Receiver);
-            ViewStatuses = new ViewStatusesCommand(BugTrackingSystem, Receiver);
-            ViewErrors = new ViewErrorsCommand(BugTrackingSystem, Receiver);
+            ViewLevelsCommand = new ViewLevelsCommand(BugTrackingSystem, Receiver);
+            ViewStatusesCommand = new ViewStatusesCommand(BugTrackingSystem, Receiver);
+            ViewErrorsCommand = new ViewErrorsCommand(BugTrackingSystem, Receiver);
 
-            InsertCriticallyLevel = new InsertCriticallyLevelCommand(BugTrackingSystem, Receiver);
-            InsertError = new InsertErrorCommand(BugTrackingSystem, Receiver);
-            InsertStatus = new InsertStatusCommand(BugTrackingSystem, Receiver);
+            InsertCriticallyLevelCommand = new InsertCriticallyLevelCommand(BugTrackingSystem, Receiver);
+            InsertErrorCommand = new InsertErrorCommand(BugTrackingSystem, Receiver);
+            InsertStatusCommand = new InsertStatusCommand(BugTrackingSystem, Receiver);
 
-            EditCriticallyLevel = new EditCriticallyLevelCommand(BugTrackingSystem, Receiver);
-            EditError = new EditErrorCommand(BugTrackingSystem, Receiver);
-            EditStatus = new EditStatusCommand(BugTrackingSystem, Receiver);
+            EditCriticallyLevelCommand = new EditCriticallyLevelCommand(BugTrackingSystem, Receiver);
+            EditErrorCommand = new EditErrorCommand(BugTrackingSystem, Receiver);
+            EditStatusCommand = new EditStatusCommand(BugTrackingSystem, Receiver);
 
-            RemoveCriticallyLevel = new RemoveCriticallyLevelCommand(BugTrackingSystem, Receiver);
-            RemoveError = new RemoveErrorCommand(BugTrackingSystem, Receiver);
-            RemoveStatus = new RemoveStatusCommand(BugTrackingSystem, Receiver);
+            RemoveCriticallyLevelCommand = new RemoveCriticallyLevelCommand(BugTrackingSystem, Receiver);
+            RemoveErrorCommand = new RemoveErrorCommand(BugTrackingSystem, Receiver);
+            RemoveStatusCommand = new RemoveStatusCommand(BugTrackingSystem, Receiver);
+
+            FindErrorsCommand = new FindErrorsCommand(BugTrackingSystem, Receiver);
+            FindStatusesCommand = new FindStatusesCommand(BugTrackingSystem, Receiver);
+            FindLevelsCommand = new FindLevelsCommand(bugTrackingSystem, Receiver);
         }
 
         public void SetBugTrackingSystem(BugTrackingSystem bugTrackingSystem) => BugTrackingSystem = bugTrackingSystem;
